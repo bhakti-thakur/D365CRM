39fc042b0b39e5cf300f8cd835c9bf50d4ab6096
bhakti-thakur <tbhakti274@gmail.com>
2025-06-30 13:33:50 +0530

    Finally working FILTERING LOOKUPS BASED ON ANOTHER LOOKUP- also CASCADING FILTERING

--- JS/filteringlookups.js
+++ JS/filteringlookups.js
@@ -4,7 +4,7 @@ function filterBrands(executionContext) {
     var autoTypelookup = formContext.getAttribute("bhaks_automobiletype").getValue()
     if (autoTypelookup != null) {
         var autoTypeId = autoTypelookup[0].id.replace(/[{}]/g, "")
-        console.log("automobile type lookup: "+ autoTypelookup[0])
+        console.log("Automobile type lookup: "+ autoTypelookup[0])
         console.log("Automobile type lookup Id:"+ autoTypeId)
         var brandControl = formContext.getControl("bhaks_brand")
         if(!brandControl){
@@ -35,4 +35,43 @@ function initializeBrandFilter(executionContext) {
     // Register for future onchange events
     autoTypeAttr.addOnChange(filterBrands);
     console.log("brand Filter initialized using initializeBrandFilter.")
+}
+
+
+function filterModels(executionContext) {
+    var formContext = executionContext.getFormContext();
+    var brandlookup = formContext.getAttribute("bhaks_brand").getValue()
+    if (brandlookup != null) {
+        var brandId = brandlookup[0].id.replace(/[{}]/g, "")
+        console.log("brand lookup: "+ brandlookup[0])
+        console.log("brand lookup Id:"+ brandId)
+        var ModelControl = formContext.getControl("bhaks_models")
+        if(!ModelControl){
+            console.error("Control 'bhaks_models' not found on the form");
+            return;
+        }
+        ModelControl.addPreSearch(function() {
+            var filterXml = 
+                `<filter type='and'>
+                    <condition attribute='bhaks_brand' operator='eq' value='${brandId}' />    
+                </filter>`
+
+            ModelControl.addCustomFilter(filterXml, "bhaks_models")
+            console.log("Models filtered!!")
+        })
+    }
+    
+}
+
+function initializeModelFilter(executionContext) {
+    var formContext = executionContext.getFormContext();
+    var brandAttr = formContext.getAttribute("bhaks_brand");
+
+    if (brandAttr && brandAttr.getValue() != null) {
+        filterModels(executionContext);
+    }
+
+    // Register for future onchange events
+    brandAttr.addOnChange(filterModels);
+    console.log("Model Filter initialized using initializeModelFilter.")
 }
